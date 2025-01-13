40f7cc8cf7a13ccf75f4541148504de85def3c00
AscianHk <jesuscereceda7@gmail.com>
2025-01-13 08:59:02 +0000

    modified:   app/ui/dashboard/sidenav.tsx

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,7 +3,7 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
-// import { usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 
 
