ddc8661df2a4472b0425a8cfb53b62f2b007369a
AscianHk <jesuscereceda7@gmail.com>
2025-01-13 08:58:29 +0000

    modified:   app/ui/dashboard/nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -7,6 +7,15 @@ import { link } from 'fs';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
 
 
  
