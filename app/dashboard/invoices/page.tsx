931fc54fbda7c146cd98121406355497d5d0c2a4
AscianHk <jesuscereceda7@gmail.com>
2025-01-13 08:53:38 +0000

    Capitulo_6

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function dashboard(){
+    return(
+        <>
+            <h1>Sigue sin poder ser lomismo</h1>
+        </>
+    );
+
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard(){
+    return (
+        <>
+            <h1>Lomismo no puede ser</h1>
+        </>
+    );
+}
